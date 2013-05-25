e82b35d63f239c207418bd9d460582a9beeb64bc
Vishvajit Singh <v.singh@utoronto.ca>
2013-05-25 00:55:54 -0700

    determine whether directory is a git directory

--- main.go
+++ main.go
@@ -1,7 +1,56 @@
 package main
 
-//import "fmt"
+import "fmt"
 import "io/ioutil"
+import "os"
+import "os/exec"
+
+type DirectoryClassification int
+
+const (
+	NotGitDirectory DirectoryClassification = iota
+	GitAutoCommitDirectory
+	GitDirtyDirectory
+	GitCleanDirectory
+)
+
+func IsRegular(m os.FileMode) bool {
+	return m & os.ModeType == 0
+}
+
+func ClassifyDirectory(path string) DirectoryClassification {
+	// it is git if:
+	// .git is a directory
+	// .git/HEAD is a file
+	// git show-ref succeeds
+
+	files, err := ioutil.ReadDir(path + "/.git")
+	if err != nil {
+		return NotGitDirectory
+	}
+
+	foundHEAD := false
+	for i := 0; i < len(files); i++ {
+		file := files[i]
+		if IsRegular(file.Mode()) && file.Name() == "HEAD" {
+			foundHEAD = true
+			break
+		}
+	}
+	if !foundHEAD {
+		return NotGitDirectory
+	}
+
+	showrefcmd := exec.Command("/usr/bin/git", "show-ref")
+	showrefcmd.Dir = path
+	runerr := showrefcmd.Run()
+	if runerr != nil {
+		return NotGitDirectory
+	}
+
+	// this is where you do more analysis
+	return GitCleanDirectory
+}
 
 func ProcessDirectory(full_path string, depth int) {
 	// the directory must be classified as either
@@ -11,7 +60,9 @@ func ProcessDirectory(full_path string, depth int) {
 	//   - dirty
 	//   - clean
 
-	
+	if ClassifyDirectory(full_path) == GitCleanDirectory {
+		fmt.Printf("found git directory: %s\n", full_path)
+	}
 }
 
 func RecurseInto(full_path string, depth int) {
